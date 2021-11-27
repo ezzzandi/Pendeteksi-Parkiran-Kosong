ba3ab103cc87f597c1699003f9628bfe59878d20
T. Ezandi Trinanda <38080159+ezzzandi@users.noreply.github.com>
2021-11-27 16:04:00 +0700

    Update and rename Parkiran_Kosong.ino to Parkiran_Kosong_Original_Code.ino
    Merapikan kode lama

--- Parkiran_Kosong/Parkiran_Kosong_Original_Code.ino
+++ Parkiran_Kosong/Parkiran_Kosong_Original_Code.ino
@@ -1,18 +1,13 @@
-#include <NewPing.h> //Library untuk Sensor Ultrasonic
-
 #define trigPin1 12 //Set Trigger di Pin digital 12
 #define echoPin1 13 //Set Echo di Pin digital 13
 #define trigPin2 8 //Set Trigger di Pin digital 8
 #define echoPin2 9 //Set Echo di Pin digital 9
-#define MAX_DISTANCE 500 //Set jarak maksimal
-NewPing sonar(trigPin1, echoPin1, MAX_DISTANCE);
-NewPing sonar1(trigPin2, echoPin2, MAX_DISTANCE);
+
 int LED1 = 2;
 int LED2 = 3;
 
-
 void setup() {
-  // initialize serial communication:
+
   pinMode(trigPin1, OUTPUT); //Set pin Trigger1 sebagai output
   pinMode(echoPin1, INPUT); //Set pin Echo1 sebagai input
   pinMode(LED1, OUTPUT); //Set LED1 sebagai output
@@ -20,8 +15,6 @@ void setup() {
   pinMode(echoPin2, INPUT); //Set pin Echo2 sebagai input
   pinMode(LED2, OUTPUT); //Set LED2 sebagai output
   Serial.begin(9600);
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
 }
 
 void loop() {
@@ -33,7 +26,7 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin1, LOW);
   duration = pulseIn(echoPin1, HIGH);
-  jarak = (duration/2) / 29.1;
+  jarak = (duration / 2) / 29.1;
 
   int duration1, jarak1;
   digitalWrite(trigPin2, LOW);
@@ -42,25 +35,25 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin2, LOW);
   duration1 = pulseIn(echoPin2, HIGH);
-  jarak1 = (duration1/2) / 29.1;
-  
+  jarak1 = (duration1 / 2) / 29.1;
+
   Serial.print(jarak);
   Serial.print(" cm  ");
   Serial.print(jarak1);
   Serial.println(" cm");
 
-  if(jarak<=15){ // Jarak (Cm) dapat anda sesuaikan
+  if (jarak <= 15) { // Jarak (Cm) dapat anda sesuaikan
     digitalWrite(LED1, LOW); //LED1 mati
     delay(450); //Delay
-  }  
-  else{ //Jika jarak lebih dari yang ditentukan
-  digitalWrite(LED1, HIGH); //LED1 hidup
   }
-  
-  if(jarak1<=15){ // Jarak (Cm) dapat anda sesuaikan
+  else { //Jika jarak lebih dari yang ditentukan
+    digitalWrite(LED1, HIGH); //LED1 hidup
+  }
+
+  if (jarak1 <= 15) { // Jarak (Cm) dapat anda sesuaikan
     digitalWrite(LED2, LOW); //LED2 mati
-  }else{
-  digitalWrite(LED2, HIGH); //LED2 hidup
+  } else {
+    digitalWrite(LED2, HIGH); //LED2 hidup
   }
   delay(450); //Delay
-  }
+}
